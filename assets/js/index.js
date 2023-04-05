c3a583d6a8095e8e6d2473a268a8bbf34428900c
ukpatell <umangpatelr4@gmail.com>
2023-04-05 11:29:28 -0400

    Initial Load

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const playButton = document.getElementById('play-button');
+const playContainer = document.getElementById('play-container');
+const gameContainer = document.getElementById('container');
+const gameTitle = document.querySelectorAll(".title");
+
+
+playButton.addEventListener('click', () => {
+  playContainer.style.display = 'none';
+  for (let i = 0; i < gameTitle.length; i++) {
+    gameTitle[i].style.color = 'black';
+  }
+  gameContainer.style.display = 'block';
+
+
+  // call function to initialize game here
+});
